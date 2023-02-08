0396ac11c93b36e99116edb79a1b4046a7a8d083
r-chong <mubblybub@gmail.com>
2023-02-07 22:00:33 -0500

    added Assignment 3 -unfinished

---
+++ ChongAssignment3/src/chongassignment3/ChongAssignment3.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chongassignment3;
+
+import java.util.Scanner;
+/**
+ *
+ * @author s201076119
+ */
+public class ChongAssignment3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        String[] catalog = {"Peanuts","Treenuts","Walnuts"};
+        
+                
+        for (String i : catalog) {
+          System.out.println(i);
+        }
+    }
+    
+}
