1cf84252a127b4d51e1c7acc024d04adc6d1b5c6
Reese Chong <S201076119@ddsbstudent.ca>
2023-04-27 12:37:31 -0400

    assignmetn1 16]

---
+++ ChongAssignment16/src/chongassignment16/ChongAssignment16.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chongassignment16;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author s201076119
+ */
+public class ChongAssignment16 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // declare and initialize constants which are also arrays
+        final String [] WORD_LIST = {"addition","comparison","advice","appearance","philosophy","security","world","negotiation","quantity","speaker","unit","basis","attitude","combination","data","reputation","dad","library","grocery","insect"};
+
+        // declare and initialize 10 variables
+        String currentWord;
+        
+        
+        int wordLength = currentWord.length();
+        
+        String [] guessedLetters = new String [wordLength];
+        
+        // declare and initialize scanner
+        Scanner keyedInput = new Scanner (System.in);
+        
+        
+        
+        
+//        10 Significant Variables (5 marks)
+//        3 Significant Constant (3 marks)
+//        2 Unique and Necessary Loops (10 marks)
+//        Not just 2 loops that calculate 2 totals
+//        At least one while loop or do while loop (2 marks)
+//        2 Necessary Arrays (10 marks) 
+//        At least 40 Elements (data) in total (5 marks)
+//        Commenting (5 marks)
+//        Creative, Original and User-friendly (30 marks)
+//        No GUI needed (does not work with while loops)
+
+    }
+    
+}
