27c4ccfe915aa41f8244e95535456c89f7f05692
Reese Chong <dev.reese.chong@gmail.com>
2023-06-12 09:43:05 -0400

    finished a20)

--- ChongAssignment20/src/chongassignment20/ChongAssignment20.java
+++ ChongAssignment20/src/chongassignment20/ChongAssignment20.java
@@ -28,12 +28,12 @@ public class ChongAssignment20 {
     public static void menuOut() {
         System.out.println("---------------------------------");
         System.out.println("Welcome to the Calculationator!");
-        System.out.println("Please choose 1 option:");
-        System.out.println("1... Find side C in pythagorean theorem");
-        System.out.println("2... Find F in F = ma");
-        System.out.println("3... Find");
-        System.out.println("4... Find");
-        System.out.println("5... Find");
+        System.out.println("Pleasec hoose 1 option:");
+        System.out.println("1... Find C in pythagorean theorem, given a & b");
+        System.out.println("2... Find F in F = ma equation, given m & a");
+        System.out.println("3... Find modulus (n1 % n2)");
+        System.out.println("4... Find E in E=mc^2 equation, given m");
+        System.out.println("5... Find the log given base & value");
         System.out.println("6... Exit Program");
         System.out.println("Please choose 1 option:");
         System.out.println("---------------------------------");
@@ -44,52 +44,54 @@ public class ChongAssignment20 {
         // ask for inputs in double form
         System.out.println("Please enter the first number");
         num1 = keyedInput.nextDouble();
-        System.out.println("Please enter the second number");
+        System.out.println("Please enter the second number (enter 0 if not needed)");
         num2 = keyedInput.nextDouble();
     }
     
-    public static void outputAnswer(String ans) {
-        System.out.println(ANSI_YELLOW_BACKGROUND + "The answer is " + ans + ANSI_RESET);
-        return;
-    };
-    
     public static String formatString(double n1) {
+        // this is a subroutine that I created so that I could format all numbers into strings that are rounded!
+        // it is probably not necessary for this program, however I would do this in the future to save code
         n1 = Math.round(n1);
         String s = Double.toString(n1);
         return s;
     }
     
+    public static void outputAnswer(String ans) {
+        // subroutine to return nice-looking strings
+        System.out.println(ANSI_YELLOW_BACKGROUND + "The answer is " + ans + ANSI_RESET);
+        return;
+    };
+    
     public static double pythagTheorem(double n1, double n2) {
-        // c^2 = a^2 + b^2
+        // input into equation c^2 = a^2 + b^2
         double n3 = Math.pow(n1,2) + Math.pow(n2,2);
         n3 = Math.sqrt(n3);
         return n3;
     };
     
     public static double forceEquation(double m, double a) {
-        // F = ma
+        // input into equation F = ma
         double f = m * a;
         return f;
     };
     
-    public static double energyEquation(double m, double c) {
-        // E = mc^2
-        double e = m * Math.pow(c,2);
+    public static double energyEquation(double m) {
+        // input into equation E = mc^2
+        double e = m * Math.pow(300000000,2);
         return e;
     };
     
-    public static double modulusEquation(double n1, double n2) {
-        // E = mc^2
+    public static double modulus(double n1, double n2) {
+        // modulus is finding the remainder after division
         double n3 = n1 % n2;
         return n3;
     };
     
-    /*ideas
-Difference of two squares
-Perfect square trinomial
-Sum of interior angles of a polygon
-Pythagorean theorem
-    */
+    public static double logarithm(double n1, double n2) {
+        // divide log of value by log of base
+        double n3 = Math.log(n2) / Math.log(n1);
+        return n3;
+    };
     
     /**
      * @param args the command line arguments
@@ -102,8 +104,9 @@ Pythagorean theorem
         
         // main program loop (make multiple calculations)
         do {
+            // run menu subroutine
             menuOut();
-
+            
             // ask user for their choice
             choice = keyedInput.nextInt();
             
@@ -114,6 +117,7 @@ Pythagorean theorem
                 break;
             }
             
+            // collect user input, store in public static (accessible everywhere)
             userCollectData();
             
             // depending on choice, run the correct subroutine
@@ -122,15 +126,20 @@ Pythagorean theorem
                     outputAnswer(formatString(pythagTheorem(ChongAssignment20.num1, ChongAssignment20.num2)));
                     break;
                 case 2:
+                    outputAnswer(formatString(forceEquation(ChongAssignment20.num1, ChongAssignment20.num2)));
                     break;
                 case 3:
+                    outputAnswer(formatString(modulus(ChongAssignment20.num1, ChongAssignment20.num2)));
                     break;
                 case 4: 
+                    outputAnswer(formatString(energyEquation(ChongAssignment20.num1)));
                     break;
                 case 5:
+                    outputAnswer(formatString(logarithm(ChongAssignment20.num1, ChongAssignment20.num2)));
                     break;
             }
         } while (endProgram==false);
-        System.out.println("Thank you for using The Calculationator Program!");
+        // exit message 
+        System.out.println("Thank you for using The Calculationator! Goodbye!");
     }
 }
