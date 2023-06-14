4da64473193d614ec21c6c4aeb5ce241bb2ca31e
Reese Chong <S201076119@ddsbstudent.ca>
2023-06-14 13:49:45 -0400

    almost done a22

--- ChongAssignment22/src/chongassignment22/ChongAssignment22.java
+++ ChongAssignment22/src/chongassignment22/ChongAssignment22.java
@@ -36,30 +36,118 @@ public class ChongAssignment22 {
             tempString = keyedInput.nextLine();
             System.out.println(""); 
             
-            try {
-                if (tempString.length() >= 6) {
-                    valid = true;                
-                }
-            } 
-            catch(NumberFormatException e){ 
-                System.out.println("Sorry, that is not valid input."); 
-                System.out.println("");
+            if (tempString.length() >= 6) {
+                valid = true;
+                System.out.println("Thank you!");
+                System.out.println(tempString + " is a valid string."); 
             }
         }
-    System.out.println("Thank you!");
-    System.out.println(value + " is a valid string."); 
+    System.out.println("Let's continue with the rest of the program..."); 
+    System.out.println("..."); 
+    System.out.println("..."); 
+    
+    valid = false;
+    while (valid == false) {
+            System.out.println("Please enter a string that contains at least one lowercase “a”"); 
+            tempString = keyedInput.nextLine();
+            System.out.println(""); 
+            
+            if (tempString.contains("a")) {
+                valid = true;
+                System.out.println("Thank you!");
+                System.out.println(tempString + " is a valid string."); 
+            }
+        }
+    
+    System.out.println("Let's continue with the rest of the program..."); 
+    System.out.println("..."); 
+    System.out.println("..."); 
+    
+    valid = false;
+    while (valid == false) {
+            System.out.println("Please enter a string that string that is between 5 and 15 characters long, and does not contain the letter “z”"); 
+            tempString = keyedInput.nextLine();
+            System.out.println(""); 
+            
+            if (!tempString.contains("z")) {
+                valid = true;
+                System.out.println("Thank you!");
+                System.out.println(tempString + " is a valid string."); 
+            }
+        }
+    
+    System.out.println("Let's continue with the rest of the program..."); 
     System.out.println("..."); 
     System.out.println("..."); 
     
     valid = false;    
     while (valid == false) {
-        System.out.println("Enter an integer:"); 
+        System.out.println("Enter an integer between 5 and 500."); 
+        tempString = keyedInput.nextLine();
+        System.out.println(""); 
+
+        try {
+            value = Integer.parseInt(tempString);
+            
+            // can be modified to include 5, 500 with >= <=
+            if(value > 5 && value < 500) {
+                valid = true;
+            }
+        } 
+        catch(NumberFormatException e){ 
+            System.out.println("Sorry, that is not valid input."); 
+            System.out.println("");
+        }
+        System.out.println("Thank you!");
+        System.out.println(value + " is a valid integer."); 
+        System.out.println("..."); 
+        System.out.println("..."); 
+        }
+    
+    System.out.println("Let's continue with the rest of the program..."); 
+    System.out.println("..."); 
+    System.out.println("..."); 
+    
+    valid = false;    
+    while (valid == false) {
+        System.out.println("Enter a negative integer:"); 
+        tempString = keyedInput.nextLine();
+        System.out.println(""); 
+
+        try {
+            value = Integer.parseInt(tempString);
+            
+            if(value < 0) {
+                valid = true;
+            }
+        } 
+        catch(NumberFormatException e){ 
+            System.out.println("Sorry, that is not valid input."); 
+            System.out.println("");
+        }
+        System.out.println("Thank you!");
+        System.out.println(value + " is a valid integer."); 
+        System.out.println("..."); 
+        System.out.println("..."); 
+        }
+    
+    
+    System.out.println("Let's continue with the rest of the program..."); 
+    System.out.println("..."); 
+    System.out.println("..."); 
+    
+    valid = false;    
+    while (valid == false) {
+        System.out.println("Enter a positive, odd integer."); 
         tempString = keyedInput.nextLine();
         System.out.println(""); 
 
         try {
             value = Integer.parseInt(tempString); 
-            valid = true;
+            
+            if(value % 2 == 1 && value > 0) {
+                valid = true;
+            }
         } 
         catch(NumberFormatException e){ 
             System.out.println("Sorry, that is not valid input."); 
@@ -71,4 +159,5 @@ public class ChongAssignment22 {
         System.out.println("..."); 
         }
     }
+    }  
 }
